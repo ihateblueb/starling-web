657f323c4cc389ef870af1a27d03ae5255d7b5a3
blueb <harperssademailaddress@gmail.com>
2023-06-08 20:46:31 -0400

    added a random period to get vercel to rebuild

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     modules: ['@sidebase/nuxt-auth'],
     auth: {
         isEnabled: true,
-        // for some reason, the origin isn't regustering and causing problems in production
+        // for some reason, the origin isn't regustering and causing problems in production.
         origin: 'https://starling-beta.orchidmc.me', 
         basePath: '/api/auth',
         enableSessionRefreshPeriodically: false,
